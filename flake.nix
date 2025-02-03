09c4926974c201c5908dbd6470f12fe1853e72fd
Wolf Mermelstein <wolf@404wolf.com>
2025-02-03 17:50:20 -0500

    init

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Wolf's NixOS and Home Manager Config Overlay";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    hyprpaper.url = "github:hyprwm/hyprpaper";
+    hyprland-workspace2d.url = "github:404wolf/Hyprland-Workspace-2D";
+    nix-neovim.url = "github:404Wolf/nix-neovim";
+    capture-utils.url = "github:404Wolf/Screen-Capture";
+    dalleCLI.url = "github:404Wolf/DALLE-CLI";
+    nixGpt.url = "github:404Wolf/nixified-gpt-cli";
+    remarkable-connection-utility.url = "github:/404wolf/remarkable-connection-utility";
+    remarkable-obsidian.url = "github:404Wolf/remarkable-obsidian";
+    cartographcf.url = "github:404Wolf/CartographCF";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        overlays.default = final: prev: {
+          hyprpaper = inputs.hyprpaper.packages.${system}.default;
+          hyprland-workspace2d = inputs.hyprland-workspace2d.packages.${system}.workspace2d;
+          wrappedNvim = inputs.nix-neovim.packages.${system}.default;
+          capture-utils = inputs.capture-utils.packages.${system}.default;
+          dalleCLI = inputs.dalleCLI.packages.${system}.default;
+          nixGpt = inputs.nixGpt.packages.${system}.default;
+          rcu = inputs.remarkable-connection-utility.packages.${system}.default;
+          obsidian = inputs.remarkable-obsidian.packages.${system}.obsidian;
+          cartographcf = inputs.cartographcf.packages.${system}.default;
+        };
+
+        packages = {
+          inherit
+            (pkgs)
+            hyprpaper
+            hyprland-workspace2d
+            wrappedNvim
+            capture-utils
+            dalleCLI
+            nixGpt
+            rcu
+            obsidian
+            cartographcf
+            ;
+        };
+      }
+    );
+}
