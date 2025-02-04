3a9e0e78431e0b1f52866cada57af8b5361a3764
Wolf Mermelstein <wolf@404wolf.com>
2025-02-04 03:10:24 -0500

    dev hyprland

--- flake.nix
+++ flake.nix
@@ -5,7 +5,12 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     hyprpaper.url = "github:hyprwm/hyprpaper";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     hyprland-workspace2d.url = "github:404wolf/Hyprland-Workspace-2D";
+    hyprland-plugins.url = "github:hyprwm/hyprland-plugins";
+    hyprland-plugins.inputs.hyprland.follows = "hyprland";
+    hyprland-plugins.inputs.nixpkgs.follows = "nixpkgs";
+
     nix-neovim.url = "github:404Wolf/nix-neovim";
     capture-utils.url = "github:404Wolf/Screen-Capture";
     dalleCLI.url = "github:404Wolf/DALLE-CLI";
@@ -26,8 +31,13 @@
     flake-utils.lib.eachDefaultSystem (
       system: {
         overlays.default = final: prev: {
+          # Hyprland and related packages
+          hyprland = inputs.hyprland.packages.${system}.default;
           hyprpaper = inputs.hyprpaper.packages.${system}.default;
           hyprland-workspace2d = inputs.hyprland-workspace2d.packages.${system}.workspace2d;
+          hyprland-plugins = inputs.hyprland-plugins.packages.${system}.default;
+
+          # Other packages
           wrappedNvim = inputs.nix-neovim.packages.${system}.default;
           capture-utils = inputs.capture-utils.packages.${system}.default;
           dalleCLI = inputs.dalleCLI.packages.${system}.default;
